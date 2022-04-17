89ddea8143a47542c88f5124350c537c9d80e228
kyle <kyle.mazeika@gmail.com>
2022-04-17 10:38:38 -0400

    solve top_k_frequent_elements

--- arrays_and_hashing/top_k_frequent_elements/src/lib.rs
+++ arrays_and_hashing/top_k_frequent_elements/src/lib.rs
@@ -1,5 +1,34 @@
+use std::collections::HashMap;
+
 pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!();
+    let mut map = HashMap::new();
+    // index is the frequency a num occurs, value are a vec of nums that occur at that frequency
+    // add an extra index to offset 0, this is so the index matches the frequency a num occurs
+    // so index 1 means the nums in this vec occur 1 time
+    // without the offset, index 0 would be used to mean the nums in this vec occur 1 time
+    let mut frequency = vec![vec![]; nums.len() + 1];
+
+    for num in nums {
+        *map.entry(num).or_insert(0) += 1;
+    }
+
+    for (k, v) in map {
+        // without the offset above, would require subtracting one from the value here
+        frequency[v].push(k);
+    }
+
+    let mut result = vec![];
+
+    for i in (0..frequency.len()).rev() {
+        for n in &frequency[i] {
+            result.push(*n);
+            if result.len() as i32 == k {
+                return result;
+            }
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
