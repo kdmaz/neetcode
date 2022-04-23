ea3b2f121ece6859d793e53d13ddd55971a9a6e9
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-04-23 17:43:14 -0400

    solve palindrome

--- two_pointers/palindrome/src/lib.rs
+++ two_pointers/palindrome/src/lib.rs
@@ -1,5 +1,35 @@
 pub fn is_palindrome(s: String) -> bool {
-    todo!();
+    let mut l = 0;
+    let mut r = s.len() - 1;
+
+    let is_alpha = |l: &str| {
+        let b = l.as_bytes()[0];
+        (b >= 48 && b <= 57) || (b >= 97 && b <= 122)
+    };
+
+    while l < r {
+        let left_char = &s[l..l + 1].to_lowercase();
+        if !is_alpha(left_char) {
+            l += 1;
+            continue;
+        }
+
+        let right_char = &s[r..r + 1].to_lowercase();
+        dbg!(&is_alpha(right_char));
+        if !is_alpha(right_char) {
+            r -= 1;
+            continue;
+        }
+
+        if left_char != right_char {
+            return false;
+        }
+
+        l += 1;
+        r -= 1;
+    }
+
+    true
 }
 
 #[cfg(test)]
