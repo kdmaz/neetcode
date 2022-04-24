acaebf42282f6b9e9d52a5d449346f99693bb28b
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-04-23 20:11:33 -0400

    solves three_sum

--- two_pointers/three_sum/src/lib.rs
+++ two_pointers/three_sum/src/lib.rs
@@ -1,5 +1,35 @@
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!();
+    let mut res = vec![];
+    let mut nums = nums;
+    nums.sort();
+
+    for i in 0..nums.len() {
+        if i > 0 && nums[i] == nums[i - 1] {
+            continue;
+        }
+
+        let mut l = i + 1;
+        let mut r = nums.len() - 1;
+
+        while l < r {
+            let sum = nums[i] + nums[l] + nums[r];
+
+            if sum < 0 {
+                l += 1;
+            } else if sum > 0 {
+                r -= 1;
+            } else {
+                res.push(vec![nums[i], nums[l], nums[r]]);
+                l += 1;
+
+                while nums[l] == nums[l - 1] && l < r {
+                    l += 1;
+                }
+            }
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
