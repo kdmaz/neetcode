8baab18969fc55171b6ba08d4670409cfd03856b
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-04-26 21:33:55 -0400

    solve longest_repeating_character_replacement

--- sliding_window/longest_repeating_character_replacement/src/lib.rs
+++ sliding_window/longest_repeating_character_replacement/src/lib.rs
@@ -1,5 +1,23 @@
+use std::collections::HashMap;
+
 pub fn character_replacement(s: String, k: i32) -> i32 {
-    todo!();
+    let mut map = HashMap::new();
+    let mut longest = 0;
+    let mut l = 0;
+    let get_max = |max, v: &i32| std::cmp::max(max, *v);
+
+    for r in 0..s.len() {
+        *map.entry(&s[r..r + 1]).or_insert(0) += 1;
+
+        while ((r - l + 1) as i32 - map.values().fold(0, get_max)) > k {
+            *map.get_mut(&s[l..l + 1]).unwrap() -= 1;
+            l += 1;
+        }
+
+        longest = std::cmp::max(r - l + 1, longest);
+    }
+
+    longest as i32
 }
 
 #[cfg(test)]
