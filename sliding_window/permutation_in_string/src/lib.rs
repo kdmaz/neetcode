0880e57ce83e65d66d8948f2f51f7a889b2c87e6
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-04-27 18:00:21 -0400

    adds permutation_in_string

---
+++ sliding_window/permutation_in_string/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn check_inclusion(s1: String, s2: String) -> bool {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::check_inclusion;
+
+    #[test]
+    fn example_1() {
+        let (s1, s2) = ("ab", "eidbaooo");
+        assert_eq!(check_inclusion(s1.into(), s2.into()), true);
+    }
+
+    #[test]
+    fn example_2() {
+        let (s1, s2) = ("ab", "eidboaoo");
+        assert_eq!(check_inclusion(s1.into(), s2.into()), false);
+    }
+}
